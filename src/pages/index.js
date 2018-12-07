3876d81f3ab527860f07b928a98d50746368b419
snprabu <sn.prabu@gmail.com>
2018-12-07 08:07:21 +0530

    index page update
    index page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,15 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 
-const IndexPage = () => ( <
-    Layout >
-    <
-    h1 > Profile < /h1>  <
-    p > Welcome to your new Gatsby site. < /p> <
-    p > Now go build something great. < /p> <
-    div style = {
-        { maxWidth: '300px', marginBottom: '1.45rem' } } >
-    <
-    Image / >
-    <
-    /div> <
-    Link to = "/page-2/" > Go to page 2 < /Link> <
-    /Layout>
+const IndexPage = () => ( 
+	<Layout >
+    <h1 > Profile < /h1>  	
+	<div style = {
+        { maxWidth: '100%', marginBottom: '1.45rem' } } >
+    <Image / >
+    </div> 
+	<Link to = "/page-2/" > Go to page 2 < /Link> 
+	</Layout>
 )
 
 export default IndexPage
