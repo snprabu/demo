0d05e1c083609132652e5586a1b7d9ba29a52af7
snprabu <sn.prabu@gmail.com>
2018-12-07 08:38:17 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div>
     <div>
-      <h1 style={{ margin: 0; display: 'none'; }}>
+      <h1 style={{ margin: 0, display: 'none'; }}>
 		{siteTitle}
       </h1>
     </div>
