f3db4748b90bcb5f5bca2242bccff7e995ddbcd4
snprabu <sn.prabu@gmail.com>
2018-12-07 08:32:51 +0530

    updates
    updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => (
           style={{
             margin: '0 auto',
             maxWidth: "100%",
-            padding: '0px 1.0875rem 1.45rem',
+            padding: '0',
             paddingTop: 0,
           }}
         >
